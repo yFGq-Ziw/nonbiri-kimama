7f7b41518519ffd50d0feb590df7f116c66582e8
inari <48873950+yFGq-Ziw@users.noreply.github.com>
2023-11-24 01:51:28 +0900

    Update navi-disappear-while-scroll-and-comeback-when-scrollup.js

--- navi-disappear-while-scroll-and-comeback-when-scrollup.js
+++ navi-disappear-while-scroll-and-comeback-when-scrollup.js
@@ -1,60 +1,47 @@
-$( document )
-  .ready( function() {
-    $( '.openbtn1' )
-      .click( function() {
-        if ( $( '#g-navi' )
-          .is( ':hidden' ) ) {
-          // '#g-navi' の高さを0からautoに変更
-          $( '#g-navi' )
-            .css( 'height', '45px' );
-          // '#g-navi' をスライドダウンで表示する
-          $( '#g-navi' )
-            .slideDown();
-          $( '#g-navi' )
-            .css( 'display', 'flex' );
-          $( this )
-            .toggleClass( 'active' );
-        } else {
-          // '#g-navi' が表示されている場合
-          // '#g-navi' をスライドアップで非表示にする（アニメーション付き）
-          $( '#g-navi' )
-            .slideUp( 'slow' ); // 'slow'はアニメーションの速度を指定する例です。他にも'fast'やミリ秒単位の数値も指定可能です。
-          // '.openbtn1' から 'active' クラスを削除する
-          $( ".openbtn1" )
-            .removeClass( 'active' );
-          // スライドアップ時に高さを0に変更
-          $( '#g-navi' )
-            .css( 'height', 0 );
-        }
-      } );
-  } );
+$(document).ready(function(){
+  $(".openbtn1").click(function(){  // トグルボタンがクリックされた時
+    // メニューが非表示の場合
+    if ($("#g-navi").is(":hidden")) {
+      $("#g-navi").css("height", "45px");  // メニューの高さを45pxに設定
+      $("#g-navi").slideDown();  // メニューをスライドダウンで表示
+      $("#g-navi").css("display", "flex"); // メニューをflexボックスとして表示
+      $(this).toggleClass("active");  // トグルボタンにactiveクラスを付与
+    // メニューが表示されている場合  
+    } else {
+      $("#g-navi").slideUp("slow");  // メニューをスライドアップで非表示（アニメーション付き）
+      $(".openbtn1").removeClass("active");  // トグルボタンからactiveクラスを削除
+      $("#g-navi").css("height", 0);  // スライドアップ時にメニューの高さを0に変更
+    }
+  });
+});
+
+// スクロールアニメーション関数
 var beforePos = 0;
 
 function ScrollAnime() {
-  var scroll = $( window )
-    .scrollTop();
+  var scroll = $(window).scrollTop();
+
   // スマートフォン画面の場合はスクリプトを実行しない
-  if ( $( window )
-    .width() > 767 ) {
-    if ( scroll == beforePos ) {} else if ( 1000 > scroll || 0 > scroll - beforePos ) {
-      $( '#header' )
-        .removeClass( 'UpMove' );
-      $( '#header' )
-        .addClass( 'DownMove' );
+  if ($(window).width() > 767) {
+    if (scroll == beforePos || (1000 > scroll || 0 > scroll - beforePos)) {
+      // スクロール位置が変わっていないか、またはスクロールアップしている場合
+      $("#header").removeClass("UpMove");
+      $("#header").addClass("DownMove");
     } else {
-      $( '#header' )
-        .removeClass( 'DownMove' );
-      $( '#header' )
-        .addClass( 'UpMove' );
+      // スクロールダウンしている場合
+      $("#header").removeClass("DownMove");
+      $("#header").addClass("UpMove");
     }
     beforePos = scroll;
   }
 }
-$( window )
-  .scroll( function() {
-    ScrollAnime();
-  } );
-$( window )
-  .on( 'load', function() {
-    ScrollAnime();
-  } );
+
+// スクロールイベントにScrollAnime関数をバインド
+$(window).scroll(function() {
+  ScrollAnime();
+});
+
+// ページの読み込み時にもScrollAnime関数を実行
+$(window).on("load", function() {
+  ScrollAnime();
+});
