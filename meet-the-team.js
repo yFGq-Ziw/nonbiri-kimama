ed4f4f84e22a0c53b8954f7b5447b286654631a5
inari <48873950+yFGq-Ziw@users.noreply.github.com>
2023-12-08 04:49:46 +0900

    Update meet-the-team.js

--- meet-the-team.js
+++ meet-the-team.js
@@ -4,15 +4,15 @@ $(document)
       entries.forEach(entry => {
         if (entry.isIntersecting) {
           // Meet The Team
-          $(".about-tiles:nth-child(1)")
-            .addClass("animate__animated animate__slideInLeft");
-          $(".about-tiles:nth-child(2)")
-            .addClass("animate__animated animate__fadeInUp");
-          $(".about-tiles:nth-child(3)")
-            .addClass("animate__animated animate__slideInRight");
+          $('.about-tiles:nth-child(1)')
+            .addClass('animate__animated animate__slideInLeft');
+          $('.about-tiles:nth-child(2)')
+            .addClass('animate__animated animate__fadeInUp');
+          $('.about-tiles:nth-child(3)')
+            .addClass('animate__animated animate__slideInRight');
           // Contact
-          if (entry.target.id === "Contact") {
-            $(".top-banner1.hai")
+          if (entry.target.id === 'Contact') {
+            $('.top-banner1.hai')
               .addClass('animate__animated animate__bounceInLeft');
           }
           observer.unobserve(entry.target);
@@ -22,6 +22,6 @@ $(document)
     const observer = new IntersectionObserver(observerCallback, {
       threshold: [0, .5] // Corrected threshold value
     });
-    const elements = $("#Contact, .about-tiles");
+    const elements = $('#Contact, .about-tiles');
     elements.each((index, element) => observer.observe(element));
   });
