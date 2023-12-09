b76a85064ac7e222ea72a71ca944cd982f1681bd
inari <48873950+yFGq-Ziw@users.noreply.github.com>
2023-12-09 18:45:55 +0900

    Update meet-the-team.js

--- meet-the-team.js
+++ meet-the-team.js
@@ -1,27 +1,27 @@
-$(document)
-  .ready(function() {
-    const observerCallback = (entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          // Meet The Team
-          $('.about-tiles:nth-child(1)')
-            .addClass('animate__animated animate__slideInLeft');
-          $('.about-tiles:nth-child(2)')
-            .addClass('animate__animated animate__fadeInUp');
-          $('.about-tiles:nth-child(3)')
-            .addClass('animate__animated animate__slideInRight');
-          // Contact
-          if (entry.target.id === 'Contact') {
-            $('.top-banner1.hai')
-              .addClass('animate__animated animate__bounceInLeft');
+$(document).ready(function () {
+  const $elements = $('.top-banner1, .about-tiles');
+  const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Meet The Team
+        if (entry.target.classList.contains('about-tiles')) {
+          entry.target.classList.add('animate__animated');
+          if ($(entry.target).is(':nth-child(1)')) {
+            entry.target.classList.add('animate__slideInLeft');
+          } else if ($(entry.target).is(':nth-child(2)')) {
+            entry.target.classList.add('animate__fadeInUp');
+          } else if ($(entry.target).is(':nth-child(3)')) {
+            entry.target.classList.add('animate__slideInRight');
           }
-          observer.unobserve(entry.target);
+        // Main Category, Contact
+        } else {
+          entry.target.classList.add('animate__animated', 'animate__bounceInLeft');
         }
-      });
-    };
-    const observer = new IntersectionObserver(observerCallback, {
-      threshold: [0, .5] // Corrected threshold value
+      }
     });
-    const elements = $('#Contact, .about-tiles');
-    elements.each((index, element) => observer.observe(element));
+  };
+  const observer = new IntersectionObserver(observerCallback, {
+    threshold: [0, .5], // Corrected threshold value
   });
+  $elements.each((index, element) => observer.observe(element));
+});
