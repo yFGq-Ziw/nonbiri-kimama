3b8fba9a976ff99ccba308d63f44fb81b16e9541
inari <48873950+yFGq-Ziw@users.noreply.github.com>
2023-12-10 14:05:52 +0900

    Create .js

---
+++ .js
@@ -0,0 +1,45 @@
+$(function() {
+  $('.counter-value, .skill-percent').each(function() {
+    const target = parseInt($(this).data('target'), 10);
+    const animateFn = $(this).hasClass('counter-value') ? animateCounter : animateWidth;
+
+    new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          animateFn(entry.target, target);
+          entry.target.unobserve(entry);
+        }
+      });
+    }, { threshold: .5 }).observe(this);
+  });
+
+  function animateCounter(element, target) {
+    let currentValue = 0;
+    const step = Math.ceil(target / 50);
+
+    const interval = setInterval(() => {
+      currentValue += step;
+      $(element).text(currentValue);
+
+      if (currentValue >= target) {
+        clearInterval(interval);
+        $(element).text(target);
+      }
+    }, 30);
+  }
+
+  function animateWidth(element, target) {
+    let currentWidth = 0;
+    const step = Math.ceil(target / 50);
+
+    const interval = setInterval(() => {
+      currentWidth += step;
+      $(element).width(`${currentWidth}%`);
+
+      if (currentWidth >= target) {
+        clearInterval(interval);
+        $(element).width(`${target}%`);
+      }
+    }, 30);
+  }
+});
