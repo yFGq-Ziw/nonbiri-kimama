dea5f8e2c787b25b4aa4d04002fedfad554a9180
inari <48873950+yFGq-Ziw@users.noreply.github.com>
2023-12-11 23:03:41 +0900

    Update toppage-customize-counter.js

--- toppage-customize-counter.js
+++ toppage-customize-counter.js
@@ -2,25 +2,25 @@ $(function() {
   $('.counter-value, .skill-percent').each(function() {
     const target = parseInt($(this).data('target'), 10);
     const animateFn = $(this).hasClass('counter-value') ? animateCounter : animateWidth;
-
-    new IntersectionObserver((entries) => {
+    const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           animateFn(entry.target, target);
-          entry.target.unobserve(entry);
+          observer.unobserve(entry.target);
         }
       });
-    }, { threshold: .5 }).observe(this);
+    }, {
+      threshold: .5
+    });
+    observer.observe(this);
   });
 
   function animateWidth(element, target) {
     let currentWidth = 0;
     const step = Math.ceil(target / 50);
-
     const interval = setInterval(() => {
       currentWidth += step;
       $(element).width(`${currentWidth}%`);
-
       if (currentWidth >= target) {
         clearInterval(interval);
         $(element).width(`${target}%`);
@@ -31,11 +31,9 @@ $(function() {
   function animateCounter(element, target) {
     let currentValue = 0;
     const step = Math.ceil(target / 50);
-
     const interval = setInterval(() => {
       currentValue += step;
       $(element).text(currentValue);
-
       if (currentValue >= target) {
         clearInterval(interval);
         $(element).text(target);
