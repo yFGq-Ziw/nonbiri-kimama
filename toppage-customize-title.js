9559443f38c934454415d3f1a72b5592f145b4a1
inari <48873950+yFGq-Ziw@users.noreply.github.com>
2023-12-17 11:13:00 +0900

    Update toppage-customize-title.js

--- toppage-customize-title.js
+++ toppage-customize-title.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
-/**タイピング*/
-  "use strict";
-  var options = {
+  var typed = new Typed('.header-logo-desc p', {
     strings: ["Blog Customize", "Web Design", "Code Tips", "Idle Talk"],
-    typeSpeed: 35, // タイピングの速さ（ミリ秒）
-    backSpeed: 35, // バックスペースの速さ（ミリ秒）
-    backDelay: 2000, // テキストを全て表示した後、バックスペースが始まるまでの遅延時間（ミリ秒）
-    startDelay: 1000, // アニメーションが開始されるまでの遅延時間（ミリ秒）
+    typeSpeed: 35,
+    backSpeed: 35,
+    backDelay: 2000,
+    startDelay: 1000,
     loop: true
-  };
-  var typed = new Typed(".header-logo-desc p", options);
+  });
 });
